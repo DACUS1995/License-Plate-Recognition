fa57c3400ba846466a3cadd0b4238f32771f6f97
DACUS1995 <studormarian@gmail.com>
2021-02-09 00:01:26 +0200

    localize license plate

--- detector.py
+++ detector.py
@@ -7,11 +7,37 @@ class Detector():
 	def __init__(self):
 		pass
 
-	
 	def detect_single(self, image):
-		cv2.imshow("image", image)
+		coordinates = self.localize_plate(image)
+
+
+	def localize_plate(self, image):
+		gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+		gray_image = cv2.bilateralFilter(gray_image, 13, 15, 15)
+
+		# rectKern = cv2.getStructuringElement(cv2.MORPH_RECT, (18, 5))
+		# blackhat = cv2.morphologyEx(gray_image, cv2.MORPH_BLACKHAT, rectKern)
+
+		canny = cv2.Canny(gray_image, 170, 255, 1)
+		contours = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)[0]
+		contours = sorted(contours, key=cv2.contourArea, reverse = True)[:10]
+		screenCnt = None
+
+		for c in contours:
+			peri = cv2.arcLength(c, True)
+			approx = cv2.approxPolyDP(c, 0.018 * peri, True)
+			
+			if len(approx) == 4:
+				screenCnt = approx
+				cv2.drawContours(gray_image, [screenCnt], -1, (0, 0, 255), 3)
+				break
+
+
+		cv2.imshow("image", gray_image)
 		cv2.waitKey(0)
 
+	def extract_characters(self, plate_image):
+		pass
 
 	def preprocess_image(self, image):
 		image = image[:, :, ::-1]
